37afa9d1ba5a59225d79f2998aec7bfea33345b6
Kai Davenport <kaiyadavenport@gmail.com>
2023-11-10 18:25:01 +0000

    install ngrok

--- install.sh
+++ install.sh
@@ -33,6 +33,8 @@ sudo apt-get install -y \
   tree \
   python3.10-venv
 
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+
 sudo ln -s /usr/bin/python3 /usr/bin/python
 
 # misc
