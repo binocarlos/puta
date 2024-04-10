d25850facbdb1c329e752d6a9eff5d98fa092a41
Kai Davenport <kaiyadavenport@gmail.com>
2024-04-10 15:38:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,8 @@ sudo apt-get install -y \
   ffmpeg \
   net-tools \
   tree \
-  python3.10-venv
+  python3.10-venv \
+  libpq-dev
 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
