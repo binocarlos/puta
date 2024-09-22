b9b50b03dbb655fd824deaeea44c43b9ba77ca14
Kai Davenport <kaiyadavenport@gmail.com>
2024-09-23 00:50:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,27 +10,17 @@ sudo apt-get upgrade -y
 sudo apt-get install -y \
   tmux \
   tmate \
-  build-essential \
-  python3-setuptools \
   wget \
   tilix \
   git \
   jq \
   curl \
   kleopatra \
-  fuse \
-  libfuse2 \
   qbittorrent \
   vlc \
   gimp \
   vim \
-  python3.8 \
-  python3-pip \
-  ffmpeg \
-  net-tools \
-  tree \
-  python3.10-venv \
-  libpq-dev
+  tree
 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
