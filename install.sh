3f6e7086284af0c9ca1caa5018337e5056303213
Kai Davenport <kaiyadavenport@gmail.com>
2024-09-23 00:59:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,12 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trust
 sudo ln -s /usr/bin/python3 /usr/bin/python
 
 # docker
-curl -fsSL https://get.docker.com | sudo sh
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu noble stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker kai
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
