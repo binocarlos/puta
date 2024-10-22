74ddcc6c5452a1586eafc13f8a9e59280bd57be8
Kai Davenport <kaiyadavenport@gmail.com>
2024-10-22 11:49:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,11 @@ sudo apt-get install -y \
   vlc \
   gimp \
   vim \
-  tree
+  tree \
+  chrome-gnome-shell \
+  gnome-tweaks \
+  gnome-shell-extensions \
+  gnome-shell-extension-prefs
 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
